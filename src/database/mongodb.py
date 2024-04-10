e673de8cd33b91edcc78086559aa0063810d8bd5
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-04-10 13:51:26 +0700

    change mongodb context manager

--- src/database/mongodb.py
+++ src/database/mongodb.py
@@ -1,10 +1,14 @@
-from fastapi import Depends
+from contextlib import asynccontextmanager
+
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from src.config import Settings, get_settings
 
 
-def get_db(prop: Settings = Depends(get_settings)) -> AsyncIOMotorDatabase:
+@asynccontextmanager
+async def get_db_context(
+    prop: Settings = get_settings(),
+) -> AsyncIOMotorDatabase:
     conn = AsyncIOMotorClient(prop.database.mongo_dsn)
     try:
         yield conn.get_database(prop.database.mongo_db_name)
