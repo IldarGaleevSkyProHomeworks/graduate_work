930ab93fecaed98ce5d3efee911408605666bac4
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-04-10 13:52:11 +0700

    refactoring utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,4 @@
-from src.config import get_settings
-
-
-def get_absolute_url(
-    path: str = "",
-) -> str:
-    conf = get_settings()
-    path = path.lstrip("/")
-    return f"{conf.application.url_scheme}://{conf.application.hostname}/{path}"
+__all__ = [
+    "get_absolute_url",
+]
+from .common import get_absolute_url
