09c72d412f18a17ef91ee5bab0aa9afb92829f5d
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-04-10 12:04:52 +0700

    refactoring: fix db name

--- src/config/database.py
+++ src/config/database.py
@@ -7,4 +7,4 @@ from pydantic import (
 
 class DatabaseSettings(BaseModel):
     mongo_dsn: MongoDsn = Field(default="mongodb://localhost:27017/")
-    mongo_db_name: str = Field(default="fading_later")
+    mongo_db_name: str = Field(default="fading_letter")
